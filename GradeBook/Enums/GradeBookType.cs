f9333446a494d8d4d31cc749678b2a506889df2b
Atheek <Atheek@DESKTOP-S4QLIS6>
2021-04-03 09:10:13 +0530

    Completed module

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+   
+        enum gd
+        {
+            Standard,
+            Ranked,
+            ESNU,
+            OneToFour,
+            SixPoint
+        }
+    }
+
